8c168be87d5cb47aba9359f75b1ed255c03c5667
nothing <afamir272@gmail.com>
2021-03-12 16:59:06 +0330

    SS

---
+++ SS/README.md
@@ -0,0 +1,20 @@
+#######################################
+this is script from AmirT3rmux
+
+
+
+
+this script have realy good tools that you can intall with :
+
+#########
+python tools.py
+
+if you have problem send massage to admin
+
+telegram:@X_x_robot_x_X
+    _              _     _____ _____
+   / \   _ __ ___ (_)_ _|_   _|___ / _ __ _ __ ___  _   ___  __
+  / _ \ | '_ ` _ \| | '__|| |   |_ \| '__| '_ ` _ \| | | \ \/ /
+ / ___ \| | | | | | | |   | |  ___) | |  | | | | | | |_| |>  <
+/_/   \_\_| |_| |_|_|_|   |_| |____/|_|  |_| |_| |_|\__,_/_/\_\
+the smsBOB and god-ddoser is copy from Mr.joooooooooon
---
+++ SS/tools.py
@@ -0,0 +1,20 @@
+import os
+print("    _              _     _____ _____")
+print("   / \   _ __ ___ (_)_ _|_   _|___ / _ __ _ __ ___  _   ___  __")
+print("  / _ \ | '_ ` _ \| | '__|| |   |_ \| '__| '_ ` _ \| | | \ \/ /")
+print(" / ___ \| | | | | | | |   | |  ___) | |  | | | | | | |_| |>  <")
+print("/_/   \_\_| |_| |_|_|_|   |_| |____/|_|  |_| |_| |_|\__,_/_/\_\ ")
+print(" telegram:@X_x_robot_x_X or AmirT3rmux")
+os.system("git clone https://github.com/D4Vinci/Clickjacking-Tester")
+os.system("git clone https://github.com/abhinavkavuri/PhoneInfoga")
+os.system("git cline https://github.com/joker192/god-ddoser.git")
+os.system("git clone  https://github.com/maldevel/IPGeoLocation")
+
+
+
+print("tamom !!")
+
+
+
+print(" bazi az abzarha file hastand va baraton ersal mikonam")
+
